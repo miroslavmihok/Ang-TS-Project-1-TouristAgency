f70e5f47c8a2e3f4b500310e67abd011e1daf7d3
miroslavmihok <m.mihok1@gmail.com>
2023-10-19 17:55:03 +0200

    finished project one in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { TripsComponent } from './trips/trips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/trips/trips.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trips',
+  templateUrl: './trips.component.html',
+  styleUrls: ['./trips.component.scss']
+})
+export class TripsComponent {
+
+}
